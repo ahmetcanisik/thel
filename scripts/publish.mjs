2e3c8d2fb7d5c4b50c8ba73d8d0efd90e831113c
ahmetcanisik <can.isik.business@gmail.com>
2025-02-20 16:03:50 +0300

    changed

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,33 +1,11 @@
 #!/usr/bin/env zx
 import { $ } from 'zx';
 
-export async function to_github(commit_message = "uploaded") {
-    await $`git add --all`;
-    await $`git commit -m ${commit_message}`;
+await $`npm run build`;
 
-    const branch = (await $`git rev-parse --abbrev-ref HEAD`).stdout.trim();
-    await $`git push origin ${branch}`;
-}
+await $`git add --all`;
+await $`git commit -m 'changed'`;
 
-export async function to_npm(commit_message = "uploaded") {
-    await $`npm publish`;
-}
+await $`git push`;
 
-export async function publish(commit = "uploaded", to = "everywhere") {
-    await $`npm run build`;
-
-    if (to === "github") {
-        await to_github(commit);
-        return;
-    }
-
-    if (to === "npm") {
-        await to_npm();
-        return;
-    }
-
-    await to_github(commit);
-    await to_npm();
-}
-
-await publish();
+await $`npm run publish`;
