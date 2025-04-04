284eae7caf6ff67c686e36390b052ce8829d5edc
ahmetcanisik <can.isik.business@gmail.com>
2025-04-04 13:04:27 +0300

    v0.4.1

--- src/__tests__/pkg.test.ts
+++ src/__tests__/pkg.test.ts
@@ -1,21 +1,13 @@
+import path from 'path';
 import { GetPKGInfo } from '../helpers/pkg';
+import { readJson } from 'fs-extra';
 
 describe("GetPKGInfo", () => {
     it("should return the package.json content", async () => {
         // Act
-        const result = await GetPKGInfo();
+        const pkg = await GetPKGInfo();
+        const result = await readJson(path.join(__dirname, "..", "..", "package.json"));
         
-        // Assert
-        expect(result).toBeDefined();
-        expect(result.name).toBe("thel");
-        expect(result.version).toBe("0.4.0");
-        expect(result.description).toBe("The terminal helper");
-        expect(result.main).toBe("./dist/index.js");
-        expect(result.license).toBe("MIT");
-        
-        // Check that it has the expected structure
-        expect(result.scripts).toBeDefined();
-        expect(result.dependencies).toBeDefined();
-        expect(result.devDependencies).toBeDefined();
+        expect(pkg).toEqual(result);
     });
 });
